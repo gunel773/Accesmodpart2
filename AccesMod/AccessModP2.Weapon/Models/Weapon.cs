db930901374ede328d5b56c2ccc053f6f963fbaf
gunel773 <gunelmg@code.edu.az>
2023-11-18 12:06:07 +0400

    use regex and math methods

---
+++ AccesMod/AccessModP2.Weapon/Models/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModP2.Weapon.Models
+{
+    public class Weapon
+    {
+        public int Comb { get; set; }
+        public double TimeShoot { get; set; }
+
+        public void SingleShotWeapon(double time)
+        {
+            TimeShoot = time;
+            if (TimeShoot >= 1)
+            {
+                Console.WriteLine("This is Single shoot weapon ");
+            }
+        }
+
+        public void AutomaticWeapon(double time)
+
+        {
+            TimeShoot = time;
+            if(TimeShoot >= 0.5 && TimeShoot<1) 
+            {
+                Console.WriteLine("This is Automatic Weapon");
+            }
+        }
+
+        public void CombFilling(int comb)
+        {
+            Comb = comb;
+            if (comb == 0)
+            {
+                Console.WriteLine("Please fill the comb");
+            }
+            if (comb == 30)
+            {
+                Console.WriteLine("Comb is full");
+            }
+            else
+            {
+                Console.WriteLine("dont need fill the comb");
+            }
+        }
+    }
+}
---
+++ AccesMod/AccessModP2/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using AccessModP2.Weapon.Models;
+
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+
+#region task1 Math methods
+////Max ve Min ededin tapılması
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int max = Math.Max(x, y);
+int min = Math.Min(x, y);
+int result = max + min;
+Console.WriteLine(result);
+
+////kok alma
+double num = double.Parse(Console.ReadLine());
+double numroot = Math.Sqrt(num);
+Console.WriteLine(numroot);
+
+
+////menfi musbete cevirme bir nov modul
+decimal num1 = decimal.Parse(Console.ReadLine());
+decimal num2 = decimal.Parse(Console.ReadLine());
+decimal result = num2 - num1;
+Console.WriteLine(Math.Abs(result));
+
+////yuvarlaqladirma
+decimal z = decimal.Parse(Console.ReadLine());
+Console.WriteLine(Math.Round(z));
+
+
+
+
+
+
+#endregion
+
+#region task2 regex methods
+
+//son herfi m olani tap
+string regexPattern = @"m$";
+        Regex regex = new Regex(regexPattern);
+        string input = "ilham hasn't form";
+        Match match = regex.Match(input);
+
+if (match.Success)
+{
+    Console.WriteLine("Match found.");
+}
+else
+{
+    Console.WriteLine("No match found.");
+}
+
+
+
+
+#endregion
+
+#region Task3
+Weapon weapon = new();
+weapon.CombFilling(30);
+weapon.SingleShotWeapon(0.7);
+weapon.AutomaticWeapon(0.7);
+Console.WriteLine(weapon.CombFilling);
+#endregion
+
+
+
+
+
+
